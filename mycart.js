4ed189899ad8c04221ebaf1e7f73a7ace0846149
tm786tm <tm786tm@gmail.com>
2020-04-18 19:48:37 +0100

    cleaned up code with functions, added form to cart.html

--- mycart.js
+++ mycart.js
@@ -1,27 +1,16 @@
-
-const btn = document.createElement('button');
-const main = document.querySelector('main');
-const total = document.createElement('h3');
-const table = document.createElement('table');
-const tHeader = document.createElement('tr');
-tHeader.innerHTML = '<th>Name</th>' + '<th>Lense</th>' + '<th>Price</th>' + '<th></th>';
-table.appendChild(tHeader);
-let totalPrice = 0;
-for (let i = 0; i < localStorage.length; i++) {
-
+createTable = (i) => {
+    let data = JSON.parse(localStorage.getItem(localStorage.key(i)));
     const tRow = document.createElement('tr');
-    
     const name = document.createElement('td');
     const lense = document.createElement('td');
     const price = document.createElement('td');
     const x = document.createElement('td');
     const xButton = document.createElement('button');
-    let data = JSON.parse(localStorage.getItem(localStorage.key(i)));
 
-    
     table.setAttribute('id', 'table');
     table.classList.add('table');
     main.appendChild(table);
+
     xButton.classList.add('btn', 'btn-danger');
 
     name.innerHTML = data.name;
@@ -29,27 +18,24 @@ for (let i = 0; i < localStorage.length; i++) {
     price.innerHTML = '$' + data.price / 100;
     x.innerHTML = '';
     xButton.innerHTML = 'X';
-
     x.appendChild(xButton);
-    btn.classList.add('btn', 'btn-secondary', 'w-25', 'mx-auto', 'mb-3');
+
+    emptyButon.classList.add('btn', 'btn-secondary', 'w-25', 'mx-auto', 'mb-3');
 
     totalPrice += data.price;
     total.classList.add('text-center');
     total.innerHTML = 'Total Price is - $' + totalPrice / 100;
     main.appendChild(total);
 
-    btn.innerHTML = 'Empty Cart';
-    main.appendChild(btn);
+    emptyButon.innerHTML = 'Empty Cart';
+    //main.appendChild(emptyButon);
 
-    btn.addEventListener('click', () => {
+    emptyButon.addEventListener('click', () => {
         window.localStorage.clear();
         table.remove();
         main.innerHTML = '<h2 class = "text-center">Your Shopping Cart is Currently Empty</h2>';
-        
-
     });
     xButton.addEventListener('click', () => {
-        console.log('removing ' + window.localStorage.key(i))
         localStorage.removeItem(localStorage.key(i));
         xButton.parentElement.parentElement.remove();
         location.reload();
@@ -58,12 +44,40 @@ for (let i = 0; i < localStorage.length; i++) {
     tRow.appendChild(lense);
     tRow.appendChild(price);
     tRow.appendChild(x);
+    return tRow;
+}
 
+tableHeadings = () => {
+    if (localStorage.length == 0) {
+        document.getElementById('orderSection').remove();
+        main.innerHTML = '<h2 class = "text-center">Your Shopping Cart is Currently Empty</h2>';
+    } else {
+        const heading = document.createElement('h2');
+        heading.innerHTML = 'Items in Your Cart';
+        heading.classList.add('text-center')
+        main.parentNode.insertBefore(heading, main);
+        heading.parentNode.insertBefore(br, heading);
+        const tHeader = document.createElement('tr');
+        tHeader.innerHTML = '<th>Name</th>' + '<th>Lense</th>' + '<th>Price</th>' + '<th></th>';
+        table.appendChild(tHeader);
+    }
+}
+
+const emptyButon = document.createElement('button');
+const main = document.querySelector('main');
+const total = document.createElement('h5');
+const table = document.createElement('table');
+const br = document.createElement('br');
+
+tableHeadings();
+
+let totalPrice = 0;
+for (let i = 0; i < localStorage.length; i++) {
+    const tRow = createTable(i);
     table.appendChild(tRow);
+}
 
-}
-if (localStorage.length == 0) {
-    main.innerHTML = '<h2 class = "text-center">Your Shopping Cart is Currently Empty</h2>';
-}
+
+
 
 
